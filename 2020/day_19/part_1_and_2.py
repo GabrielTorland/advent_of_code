f859f38b2f4a7d2228db48946d911144e5042734
Gabriel <73033765+GabrielTorland@users.noreply.github.com>
2022-04-24 18:28:40 +0200

    day_19👌

---
+++ 2020/day_19/part_1_and_2.py
@@ -0,0 +1,31 @@
+from operator import ge
+import sys, re
+
+
+infile = sys.argv[1] if len(sys.argv) > 1 else "input.in"
+rules_raw, msgs = open(infile).read().split("\n\n")
+
+rules = {}
+for rule in rules_raw.split("\n"):
+    num, r = rule.split(": ")
+    rules[num] = [s.split() for s in r.split(" | ")]
+
+# Creating regular expression
+def gen_regex(r='0', depth=15):
+    if depth == 0:
+        return ""
+    if rules[r][0][0].startswith('"'):
+        return rules[r][0][0].strip('""')
+    return "(" + "|".join(["".join([gen_regex(sub, depth-1) for sub in subrule]) for subrule in rules[r]]) + ")"
+
+# Part 1 
+r1 = re.compile(gen_regex("0"))
+res = [r1.fullmatch(msg) for msg in msgs.split('\n')]
+print(f"part 1: {len([x for x in res if x])}")
+rules['8'] = [['42'], ['42', '8']]
+
+# part 2
+rules['11'] = [['42', '31'], ['42', '11', '31']]
+r2 = re.compile(gen_regex("0"))
+res = [r2.fullmatch(msg) for msg in msgs.split('\n')]
+print(f"part 2: {len([x for x in res if x])}")
