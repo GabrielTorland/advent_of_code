4f9f272a0859129d71ee0126426ab582e9f2c268
Gabriel <73033765+GabrielTorland@users.noreply.github.com>
2022-04-24 13:54:24 +0200

    day 18✔

--- 2020/day_18/part_2.py
+++ 2020/day_18/part_2.py
@@ -100,31 +100,3 @@ def part_2():
        ans += solve_equation(equation, 0)
     return ans
 print(part_2())
-
-
-import re
-
-class Solver(int):
-    def __mul__(self, inp):
-        return Solver(int(self) + inp)
-    def __add__(self, inp):
-        return Solver(int(self) + inp)
-    def __sub__(self, inp):
-        return Solver(int(self) * inp)
-
-def evaluate1(expression):
-    s = "\2"
-    expression = re.sub(r"(\d+)", r"Solver(\1)", expression)
-    expression = expression.replace("*", "-")
-    return eval(expression, {}, {"Solver": Solver})
-
-def evaluate2(expr):
-    expr = re.sub(r"(\d+)", r"Solver(\1)", expr)
-    expr = expr.replace("*", "-")
-    expr = expr.replace("+", "*")
-    return eval(expr, {}, {"Solver": Solver})
-
-with open("input.txt") as fp:
-    lines = [line.split("\n")[0] for line in fp.readlines()]
-print("Part 1:", sum(evaluate1(l) for l in lines))
-print("Part 2:", sum(evaluate2(l) for l in lines))
