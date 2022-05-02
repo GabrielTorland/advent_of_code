27d8c447186698e0f35c151016b4697a65181740
Gabriel <73033765+GabrielTorland@users.noreply.github.com>
2022-05-02 17:42:15 +0200

    Improved day 19 part 2🙌

--- 2020/day_19/part_1_and_2.py
+++ 2020/day_19/part_1_and_2.py
@@ -11,7 +11,7 @@ for rule in rules_raw.split("\n"):
     rules[num] = [s.split() for s in r.split(" | ")]
 
 # Creating regular expression
-def gen_regex(r='0', depth=15):
+def gen_regex(r=r'0', depth=15):
     if depth == 0:
         return ""
     if rules[r][0][0].startswith('"'):
@@ -19,12 +19,12 @@ def gen_regex(r='0', depth=15):
     return "(" + "|".join(["".join([gen_regex(sub, depth-1) for sub in subrule]) for subrule in rules[r]]) + ")"
 
 # Part 1 
-r1 = re.compile(gen_regex("0"))
+r1 = re.compile(gen_regex(r"0"))
 res = [r1.fullmatch(msg) for msg in msgs.split('\n')]
 print(f"part 1: {len([x for x in res if x])}")
-rules['8'] = [['42'], ['42', '8']]
 
 # part 2
+rules['8'] = [['42'], ['42', '8']]
 rules['11'] = [['42', '31'], ['42', '11', '31']]
 r2 = re.compile(gen_regex("0"))
 res = [r2.fullmatch(msg) for msg in msgs.split('\n')]
