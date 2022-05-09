35c4ce1ad8200670f0c2bda0ece8eba1ced657f4
Gabriel <73033765+GabrielTorland@users.noreply.github.com>
2022-05-09 23:34:40 +0200

    day 21 🙌

--- 2021/day_21/part_1.py
+++ 2021/day_21/part_1.py
@@ -1,37 +1,31 @@
 import sys
 
 def parse():
-    infile = sys.argv[1] if len(sys.argv) > 1 else 'test.in'
+    infile = sys.argv[1] if len(sys.argv) > 1 else 'input.in'
     data = open(infile, 'r').read().split('\n')
     return {"player 1": data[0][-1], "player 2": data[1][-1]}
 
-def player_move(player_position, player_score, dices_trown):
-    rng = sum(range(dices_trown+1, dices_trown+4))
-    next =  player_position+rng
-    player_position = next % 10
-    player_score += player_position+1
-    return player_position, player_score
+def player_move(ply_pos, ply_score, dices_trown):
+    rng = (dices_trown+1)*3+3
+    next =  ply_pos+rng
+    ply_pos = next % 10
+    ply_score += ply_pos+1
+    return ply_pos, ply_score
 
 
-def simulate_game(initial_positions, win_score):
-    player_1_score = 0
-    player_2_score = 0
-    player_1_position = int(inital_positions["player 1"])-1
-    player_2_position = int(initial_positions["player 2"])-1
+def simulate_game(*args):
     dices_trown = 0
-    while True:
-        player_1_position, player_1_score= player_move(player_1_position, player_1_score, dices_trown)
+    scores = [args[1], args[3]]
+    positions = [args[0], args[2]]
+    i = 0
+    while scores[(i+1)%2] < args[4]:
+        positions[i], scores[i] = player_move(positions[i], scores[i], dices_trown)
         dices_trown += 3
-        if player_1_score >= win_score:
-            return player_2_score*dices_trown
-        player_2_position, player_2_score = player_move(player_2_position, player_2_score, dices_trown)
-        dices_trown += 3
-        if player_2_score >= win_score:
-            return player_1_score*dices_trown
-
+        i = (i+1) % 2
+    return scores[i]*dices_trown 
             
 
 
 if __name__ == '__main__':
     inital_positions = parse()
-    print(simulate_game(inital_positions, 1000))
+    print(simulate_game(int(inital_positions["player 1"])-1, 0, int(inital_positions['player 2'])-1, 0, 1000))
