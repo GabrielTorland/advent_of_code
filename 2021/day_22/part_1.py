8f731369caddecb3f4dda754103f12de8a370b60
Gabriel <73033765+GabrielTorland@users.noreply.github.com>
2022-05-10 12:53:09 +0200

    day 22 in progress

---
+++ 2021/day_22/part_1.py
@@ -0,0 +1,14 @@
+import sys
+from collections import defaultdict, Counter
+
+
+def parse():
+    infile = sys.argv[1] if len(sys.argv) > 1 else 'input.in'
+    list
+    with open(infile, 'r') as f:
+        for line in f.readlines():
+            data = line.strip().split(' ')
+            if data[0] == 'on':
+
+if __name__ == '__main__':
+    parse()
