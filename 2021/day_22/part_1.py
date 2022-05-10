6161ed52f2cab5e91196998d56d16af136a8ef8c
Gabriel Torland <gabri.torland@gmail.com>
2022-05-10 16:41:13 -0400

    day 22 progress

--- 2021/day_22/part_1.py
+++ 2021/day_22/part_1.py
@@ -4,11 +4,27 @@ from collections import defaultdict, Counter
 
 def parse():
     infile = sys.argv[1] if len(sys.argv) > 1 else 'input.in'
-    list
+    cubes = set()
     with open(infile, 'r') as f:
         for line in f.readlines():
             data = line.strip().split(' ')
+            new_cubes = set()
+            tmp_cubes = [list(range(int(rng[2:].split("..")[0]), int(rng[2:].split("..")[1])+1)) for rng in data[1].split(',')]
+            new_cubes = {(int(tmp_cubes[0][i]), int(tmp_cubes[1][i]), int(tmp_cubes[2][i])) for i in range(len(tmp_cubes[0]))}
             if data[0] == 'on':
+                cubes = cubes.union(new_cubes)
+            else:
+                cubes = cubes-new_cubes
+    return cubes
+                
+def count_cubes(cubes):
+    in_range = set()
+    for x, y, z in cubes:
+        if -50 <= x <= 50 and -50 <= y <= 50 and -50 <= z <= 50:
+            in_range.add((x, y, z))
+    return len(in_range)
 
 if __name__ == '__main__':
-    parse()
+    cubes = parse()
+    print("Part 1:", count_cubes(cubes))
+
