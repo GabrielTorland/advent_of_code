bb4e8a410c6e1d3af38fcca3de4706b9bfc9901c
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-05-27 23:29:16 +0200

    In progress

--- 2021/day_19/part_1.py
+++ 2021/day_19/part_1.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
-import math
+from dis import dis
 from itertools import permutations
+import math
+import sys
 
 # Each scanner is capable of detecting all beacons in a large cube centered on the scanner; 
 # beacons that are at most 1000 units away from the scanner in each of the three axes (x, y, and z) have their precise position determined relative to the scanner.
@@ -15,15 +17,16 @@ class Scanner:
         self.pos = None # Unknown
         self.overlapping = list() # Unknown
         self.overlapping_beacons = list() # Unknown
+        self.validated = False
 
     def calculate_distances(self):
         for i in range(len(self.beacons)):
             for j in range(len(self.beacons)):
                 if j > i:
-                    self.distances[math.sqrt((self.beacons[i][0] - self.beacons[j][0])**2 + (self.beacons[i][1] - self.beacons[j][1])**2 + (self.beacons[i][2] - self.beacons[j][2])**2)].append(self.beacons[i])
-                    self.distances[math.sqrt((self.beacons[i][0] - self.beacons[j][0])**2 + (self.beacons[i][1] - self.beacons[j][1])**2 + (self.beacons[i][2] - self.beacons[j][2])**2)].append(self.beacons[j])
+                    self.distances[math.sqrt((self.beacons[i][0] - self.beacons[j][0])**2 + (self.beacons[i][1] - self.beacons[j][1])**2 + (self.beacons[i][2] - self.beacons[j][2])**2)] += [self.beacons[i], self.beacons[j]]
 def parse():
-    with open("input.txt", 'r') as raw:
+    infile = sys.argv[1] if len(sys.argv) > 1 else "input.in"
+    with open(infile, 'r') as raw:
         index = 0
         scanners = list()
         for line_ in raw.readlines():
@@ -38,7 +41,7 @@ def parse():
                 continue
             pos = line.split(',')
             scanner.beacons.append((int(pos[0]), int(pos[1]), int(pos[2])))
-
+        scanners[0].pos = (0, 0, 0)
         return scanners
 
 def region_grouping(scanners):
@@ -50,34 +53,44 @@ def region_grouping(scanners):
             if len(overlapping) >= 12: 
                 scanners[i].overlapping_beacons.append(overlapping)
                 scanners[j].overlapping_beacons.append(overlapping)
-                scanners[i].overlapping.append(scanners[i])
-                scanners[j].overlapping.append(scanners[j])
+                scanners[i].overlapping.append(scanners[j])
+                scanners[j].overlapping.append(scanners[i])
     return scanners
 
-def count_beacons(scanner, pos):
-    beacons = set()
-    comparisons = []
-    perms = []
-    for perm in list(permutations(['x', 'y', 'z', '-x', '-y', '-z'], 3)):
-        # Count occurrences of char in list
-        if perm.count('x')+perm.count('-x') > 1:
-            continue
-        if perm.count('y')+perm.count('-y') > 1:
-            continue
-        if perm.count('z')+perm.count('-z') > 1:
-            continue
-        perms.append(perm)
-    for i, ov_scanner in enumerate(scanner.overlapping):
-        for dist in scanner.overlapping_beacons[i]:
-            comparisons.append([])
-            comparisons[i].append(scanner.distances[dist])
-            comparisons[i].append(ov_scanner.distances[dist])
-        for perm in perms:
+def calculate_distance(x_1, x_0, y_1, y_0, z_1, z_0):
+    distance = math.sqrt((x_1 - x_0)**2 + (y_1 - y_0)**2 + (z_1 - z_0)**2)
+    return distance
 
-            for j in range(len(comparisons[i])):
-                for k in range(len(comparisons[i][j])):
-                   pass
-                   
+def reconstruct(scanner):
+    not_validated = 0
+    scanner.validated = True
+    for i, scanner_ in enumerate(scanner.overlapping):
+        if not scanner.validated:
+            not_validated += 1
+            continue
+        orientations = list(permutations([1, 2, 3]))+list(permutations([-1, 2, 3]))+list(permutations([1, -2, 3]))+list(permutations([1, 2, -3]))
+        for orientation in orientations:
+            new_overlap_beacons = list()
+            for beacon in scanner.overlapping_beacons[i]:
+                new_overlap_beacons.append(
+                    (
+                        -beacon[abs(orientation[0])-1] if orientation[0] < 0 else beacon[orientation[0]-1], 
+                        -beacon[abs(orientation[1])-1] if orientation[1] < 0 else beacon[orientation[1]-1],
+                        -beacon[abs(orientation[2])-1] if orientation[2] < 0 else beacon[orientation[2]-1]
+                    )
+                )
+            scanner.overlapping_beacons = new_overlap_beacons
+            reconstruct(scanner_)
+    if len(scanner.overlapping) == not_validated:
+        for scanner_ in scanner.overlapping:
+            reconstruct(scanner_)
 
 
-count_beacons(region_grouping(parse())[0], (0,0,0))
+
+
+
+
+if __name__ == "__main__":
+    scanners = parse()
+    scanners = region_grouping(scanners)
+    reconstruct(scanners[0])
