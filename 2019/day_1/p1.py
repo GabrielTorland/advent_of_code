f12208444cea5c144cc9637feb2c3826556dfb34
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-06-28 20:51:13 +0200

    qX

---
+++ 2019/day_1/p1.py
@@ -0,0 +1,7 @@
+import sys
+
+def part_1():
+    infile = sys.argv[1] if len(sys.argv) > 1 else "input.in"
+    return sum([int(num)//3-2 for num in open(infile).read().strip().split("\n")])
+print("Part 1: ", part_1())
+        
