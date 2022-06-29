5edfe2cb719ebbb2f97c4dae86bf052f9b5a2c27
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-06-29 15:05:56 +0200

    day 2 progess

---
+++ 2019/day_2/p2.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+
+def parse():
+    """
+    Parse the input file and return a list of lists of integers.
+    """
+    infile = sys.argv[1] if len(sys.argv) > 1 else "input.in"
+    with open(infile) as f:
+        # Convert list to numpy array
+        input = np.array([int(elem) for elem in f.read().strip().split(',')])
+        input[1] = 12
+        input[2] = 2
+        return input
+
+def part_2(input):
+    i = 0
+    while input[i] != 99:
+        if input[i] == 1:
+            input[input[i+3]] = input[input[i+1]] + input[input[i+2]]
+        elif input[i] == 2:
+            input[input[i+3]] = input[input[i+1]]*input[input[i+2]]
+        else:
+            print("Error!")
+        i += 4
+    return input[0]
+
+if __name__ == "__main__":
+    print("Part 2: ", part_2(parse()))
