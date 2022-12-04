702c827ba8e598436993a253dda1b12b5d563a4d
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-12-04 13:09:34 +0100

    update method name

--- 2022/day04/p2.py
+++ 2022/day04/p2.py
@@ -1,6 +1,6 @@
 import sys
 
-def is_subset(r_1, r_2):
+def is_in(r_1, r_2):
 	if r_1[0] >= r_2[0] and (r_1[1] <= r_2[1] or r_1[0] <= r_2[1]):
 		return 1
 	elif r_1[0] <= r_2[0] and (r_1[1] >= r_2[1] or r_1[1] >= r_2[0]):
@@ -10,4 +10,4 @@ def is_subset(r_1, r_2):
 
 if __name__ == '__main__':
 	input = [[tuple(int(n) for n in s.split("-")) for s in l.split(',')] for l in open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read().split("\n")]
-	print("Part 2: ", sum([is_subset(r[0], r[1]) for r in input]))
+	print("Part 2: ", sum([is_in(r[0], r[1]) for r in input]))
