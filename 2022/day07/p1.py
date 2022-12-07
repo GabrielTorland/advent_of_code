7e8e24305cdae4e80b0c4048680c98c61994c426
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-12-07 08:47:39 +0100

    day 7

--- 2022/day07/p1.py
+++ 2022/day07/p1.py
@@ -1,5 +1,57 @@
 import sys
-from collections import defaultdict 
+
+class Dir:
+	def __init__(self, name, parent) -> None:
+		self.name = name 
+		self.files = []
+		self.dirs = {'..': parent}
+		self.size = 0
+		self.parent = parent
+
+	def ls(self):
+		return list(self.dirs.keys()) + self.files
+	
+	def mkfile(self, file, size):
+		self.files.append((file, size))
+		self.__update_size(size)
+	def mkdir(self, dir):
+		self.dirs[dir.name] = dir
+	def __update_size(self, size):
+		self.size += size
+		if self.parent:
+			self.parent.__update_size(size)
+
+def make_fs(data):
+	fs = Dir('/', None)
+	curr = fs
+	for inst in data:
+		tmp = inst.split(' ')
+		if tmp[0] == "$":
+			if len(tmp) == 3:
+				i_1, i_2 = tmp[1], tmp[2]
+			else:
+				i_1, i_2 = tmp[1], None
+			if i_1 == "cd":
+				curr = curr.dirs[i_2]
+			elif i_1 == "ls":
+				print(curr.ls())
+		else:	
+			i_1, i_2 = tmp
+			if i_1 == "dir":
+				curr.dirs[i_2] = Dir(i_2, curr)
+			elif i_1.isnumeric():
+				curr.mkfile(i_2, int(i_1))
+	return fs
+
+def p1(dir):
+	size = 0 if dir.size > 100000 else dir.size
+	for n, d in dir.dirs.items():
+		if n == '..': continue
+		size += p1(d)
+	return size
 
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read().split('\n')[1:-1]
+	fs = make_fs(input)
+	print("Part 1: ", p1(fs))
+
