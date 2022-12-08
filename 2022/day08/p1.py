60387578e83d1ab0870f2b7ef03fdf3d6cdb0af0
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-12-08 19:42:59 +0100

    day 8

--- 2022/day08/p1.py
+++ 2022/day08/p1.py
@@ -1,5 +1,48 @@
 import sys
-from collections import defaultdict 
+from aocd import get_data, submit
 
+def visible_trees(data):
+	visible = set()
+	unknown = set()	
+	for y in range(1, len(data)-1):
+		t = -1
+		for x, col in enumerate(data[y]):
+			if col <= t: continue
+			t = col
+			visible.add((x, y))
+		t = -1	
+		for x in range(len(data[y+1])-1, -1, -1):
+			if data[y][x] <= t and (x, y) not in visible:
+				unknown.add((x, y))
+				continue
+			t = data[y][x]
+			visible.add((x, y))
+	
+	
+	for x, y in unknown:
+		vis = True 
+		for _y in range(y-1, -1, -1):
+			if data[_y][x] >= data[y][x]:
+				vis = False
+				break
+		if vis: visible.add((x, y))
+		else:
+			vis = True
+			for _y in range(y+1, len(data)):
+				if data[_y][x] >= data[y][x]:
+					vis = False
+					break
+			if vis: visible.add((x, y))
+	return visible.union(*[set((i, 0)for i in range(len(data[0]))), set((i, len(data)) for i in range(len(data[-1])))])
+
+	
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	data = [[int(c) for c in line] for line in get_data(day=8, year=2022).split('\n')]
+	# data = [[int(c) for c in line ] for line in open("test.in").read().split('\n')]
+	visible = visible_trees(data)
+	ans = len(visible)	
+	print(ans)
+	submit_ = input('Submit? (y/n)')
+	if submit_ == 's':
+		print(submit(ans), part="a", day=8, year=2022)
+		
--- 2022/day08/p2.py
+++ 2022/day08/p2.py
@@ -1,5 +1,44 @@
 import sys
-from collections import defaultdict 
+from aocd import get_data, submit
 
+def science_scores(data):
+	scores = []
+	for y in range(0, len(data)):
+		for x in range(0, len(data[y])):
+			score = 1
+			# up
+			su = 0
+			for _y in range(y-1, -1, -1):
+				su += 1
+				if data[_y][x] >= data[y][x]:
+					break
+			# down
+			sd = 0
+			for _y in range(y+1, len(data)):
+				sd += 1
+				if data[_y][x] >= data[y][x]:
+					break
+			# left
+			sl = 0
+			for _x in range(x-1, -1, -1):
+				sl += 1
+				if data[y][_x] >= data[y][x]:
+					break
+			# right
+			sr = 0
+			for _x in range(x+1, len(data[y])):
+				sr += 1
+				if data[y][_x] >= data[y][x]:
+					break	
+			scores.append(sr*sl*su*sd)
+	return scores
+	
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	data = [[int(c) for c in line] for line in get_data(day=8, year=2022).split('\n')]
+	# data = [[int(c) for c in line ] for line in open("test.in").read().split('\n')]
+	scores = science_scores(data)
+	ans = max(scores)
+	print(ans)
+	submit_ = input('Submit? (y/n)')
+	if submit_ == 's':
+		print(submit(ans), part="b", day=8, year=2022)
