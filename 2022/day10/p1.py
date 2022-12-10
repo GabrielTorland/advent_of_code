f215f41bba05c122086d059f4cc82d57a0dbbc4a
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-12-10 12:56:15 +0100

    day 10

--- 2022/day10/p1.py
+++ 2022/day10/p1.py
@@ -1,5 +1,28 @@
-import sys
-from collections import defaultdict 
+from aocd import get_data
+
+def calc_signal_strengths(inst):
+	# storing the instructions with the associated starting cycle
+	oper = []
+	for i in inst:
+		match i.split():
+			case ["noop", *_]:
+				oper.append((0, 2 if len(oper) == 0 else oper[-1][1] + 1))
+			case ["addx", rest]:
+				oper.append((int(rest), 3 if len(oper) == 0 else oper[-1][1] + 2))
+	
+	sign_strengths = []
+	cycle = 1
+	# current signal strength
+	X = 1
+	while len(oper) > 0:
+		cycle += 1
+		# check if we are done with the current instruction, move to the next one
+		if cycle == oper[0][1]:
+			X += oper.pop(0)[0]
+		if cycle in [20, 60, 100, 140, 180, 220]:
+			sign_strengths.append(X*cycle)
+	return sign_strengths
 
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	inst = get_data(day=10, year=2022).splitlines()
+	print("Part 1: ", sum(calc_signal_strengths(inst)))
--- 2022/day10/p2.py
+++ 2022/day10/p2.py
@@ -1,5 +1,41 @@
-import sys
-from collections import defaultdict 
+from aocd import get_data
+
+def calc_signal_strengths(instructions):
+	# storing the instructions with the associated starting cycle
+	oper = []
+	for instruction in instructions:
+		match instruction.split():
+			case ["noop", *_]:
+				oper.append((0, 2 if len(oper) == 0 else oper[-1][1] + 1))
+			case ["addx", rest]:
+				oper.append((int(rest), 3 if len(oper) == 0 else oper[-1][1] + 2))
+	
+	signal_strengths = []
+	cycle = 1
+	# current signal strength
+	X = 1
+	# list of string containing the pattern we are asked to find
+	patter = []
+	# tmp string that is appended to patter, i.e., a row of the pattern
+	row = ""
+	while len(oper) > 0:
+		# Check if the spite is on the current entry 
+		if X <= len(row)+1 <= X + 2:
+			row += "#"
+		else:
+			row += "."
+		cycle += 1
+		# check if we are done with the current instruction, move to the next one
+		if cycle == oper[0][1]:
+			X += oper.pop(0)[0]
+		if cycle in [41, 81, 121, 161, 201, 241]:
+			signal_strengths.append(X*cycle)
+			patter.append(row)
+			row = ""
+	return signal_strengths, patter
 
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	inst = get_data(day=10, year=2022).splitlines()
+	signal_strengths, pattern = calc_signal_strengths(inst)
+	print("Part 2: ")
+	for line in pattern: print(line)
