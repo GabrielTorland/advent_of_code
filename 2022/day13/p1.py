b0946a5a8e05872c73b3f85e533b2d7c69893676
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-12-13 13:11:33 +0100

    day 13

--- 2022/day13/p1.py
+++ 2022/day13/p1.py
@@ -1,5 +1,38 @@
-import sys
-from collections import defaultdict 
+from aocd import get_data
+import json
+
+def comp_elems(a, b, n):
+	for i in range(min(len(a), len(b))):
+		e1, e2 = a[i], b[i]
+		# both elements are ints
+		if isinstance(e1, int) and isinstance(e2, int):
+			if e1 > e2: return 0
+			elif e1 < e2: return n 
+		# element 1 is an int and element 2 is a list
+		elif isinstance(e1, int) and isinstance(e2, list): 
+			state = comp_elems([e1], e2, n)
+			if state == 0: return 0
+			elif state == n: return n 
+		# element 1 is a list and element 2 is an int
+		elif isinstance(e1, list) and isinstance(e2, int): 
+			state = comp_elems(e1, [e2], n)
+			if state == 0: return 0
+			elif state == n: return n 
+		# both elements are lists
+		elif isinstance(e1, list) and isinstance(e2, list): 
+			state = comp_elems(e1, e2, n)
+			if state == 0: return 0
+			elif state == n: return n 
+	if len(a) == len(b):
+		return -1
+	elif len(a) < len(b):
+		return n
+	else:
+		return 0
 
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	data = [[json.loads(elem) for elem in pair.split('\n')] for pair in get_data(day=13, year=2022).split("\n\n")]
+	right_order = 0
+	for i, pair in enumerate(data):
+		right_order += comp_elems(pair[0], pair[1], i+1)
+	print("Part 1: ", right_order)
--- 2022/day13/p2.py
+++ 2022/day13/p2.py
@@ -1,5 +1,65 @@
-import sys
-from collections import defaultdict 
+from aocd import get_data
+import json
+class Packet:
+	def __init__(self, value):
+		self.id = self.__create_tuple(value)
+		self.value = value
+
+	def __create_tuple(self, _value):
+		value = _value.copy()
+		for i, elem in enumerate(value):
+			if isinstance(elem, list):
+				value[i] = self.__create_tuple(elem)
+		return tuple(value)
+
+	def __hash__(self):
+		return self.id
+
+	def __comp_elems(self, a, b, out):
+		if a == [[1], 4]:
+			print()
+		for i in range(min(len(a), len(b))):
+			e1, e2 = a[i], b[i]
+			# both elements are ints
+			if isinstance(e1, int) and isinstance(e2, int):
+				if e1 > e2: return 0 
+				elif e1 < e2: return 1 
+			# element 1 is an int and element 2 is a list
+			elif isinstance(e1, int) and isinstance(e2, list): 
+				state = self.__comp_elems([e1], e2, out)
+				if not state: return 0 
+				elif state == 1: return 1 
+			# element 1 is a list and element 2 is an int
+			elif isinstance(e1, list) and isinstance(e2, int): 
+				state = self.__comp_elems(e1, [e2], out)
+				if not state: return 0  
+				elif state == 1: return 1 
+			# both elements are lists
+			elif isinstance(e1, list) and isinstance(e2, list): 
+				state = self.__comp_elems(e1, e2, out)
+				if not state: return 0 
+				elif state == 1: return 1 
+		if len(a) == len(b):
+			return -1
+		elif len(a) < len(b):
+			return 1
+		else:
+			return 0
+	def __eq__(self, other):
+		if isinstance(other, Packet):
+			return self.id == other.id
+		else:
+			return NotImplemented
+	def __lt__(self, other):
+		return self.__comp_elems(self.value, other.value, 1) == 1
+	def __le__ (self, other):
+		return self.__comp_elems(self.value, other.value, 1) in [1, -1]
+	def __gt__(self, other):
+		return self.__comp_elems(self.value, other.value, 0) == 0
+	def __ge__(self, other):
+		return self.__comp_elems(self.value, other.value, 0) in [0, -1]
 
 if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+	packets = [Packet(json.loads(elem)) for pair in get_data(day=13, year=2022).split("\n\n") for elem in pair.split('\n')] + [Packet([[2]]), Packet([[6]])]
+	packets.sort()	
+	print("Part 2: ", (packets.index(Packet([[2]])) + 1)*(packets.index(Packet([[6]])) + 1))
