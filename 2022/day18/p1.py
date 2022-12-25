50d74f203f7ebb8e114d6837ec4c8a38318a8fa6
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2022-12-25 16:00:37 +0100

    day 18

--- 2022/day18/p1.py
+++ 2022/day18/p1.py
@@ -1,5 +1,22 @@
-import sys
-from collections import defaultdict 
+from aocd import get_data
+from collections import defaultdict
 
-if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+
+if __name__ == "__main__":
+	lava_droplets = [tuple(int(elem) for elem in coord.split(',')) for coord in get_data(day=18, year=2022).splitlines()]
+	world = defaultdict(int)
+	sides = 0
+	for x, y, z in lava_droplets:
+		world[(x, y, z)] = 1
+		sides += -1 if world[(x+1, y, z)] else 1
+		sides += -1 if world[(x-1, y, z)] else 1
+		sides += -1 if world[(x, y+1, z)] else 1
+		sides += -1 if world[(x, y-1, z)] else 1
+		sides += -1 if world[(x, y, z+1)] else 1
+		sides += -1 if world[(x, y, z-1)] else 1
+	print("Part 1: ", sides)
+		
+
+
+
+		
--- 2022/day18/p2.py
+++ 2022/day18/p2.py
@@ -1,5 +1,62 @@
+from aocd import get_data
 import sys
-from collections import defaultdict 
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
 
-if __name__ == '__main__':
-	input = open(sys.argv[1] if len(sys.argv) > 1 else 'input.in').read()
+
+
+def lava(world, x, y, z) -> None:
+	world[x][y][z] = 2
+	if (x+1) < len(world) and world[x+1][y][z] == 0:
+		lava(world, x+1, y, z)
+	if (x-1) >= 0 and world[x-1][y][z] == 0:
+		lava(world, x-1, y, z)
+	if (y+1) < len(world[0]) and world[x][y+1][z] == 0:
+		lava(world, x, y+1, z)
+	if (y-1) >= 0 and world[x][y-1][z] == 0:
+		lava(world, x, y-1, z)
+	if (z+1) < len(world[0][0]) and world[x][y][z+1] == 0:
+		lava(world, x, y, z+1)
+	if (z-1) >= 0 and world[x][y][z-1] == 0:
+		lava(world, x, y, z-1)
+
+
+	
+
+
+if __name__ == "__main__":
+	lava_droplets = [tuple(int(elem) for elem in coord.split(',')) for coord in get_data(day=18, year=2022).splitlines()]
+
+	matplotlib.use('TKagg')
+	ax = plt.axes(projection='3d')
+
+	x_data = [x for x, y, z in lava_droplets]
+	y_data = [y for x, y, z in lava_droplets]
+	z_data = [z for x, y, z in lava_droplets]
+	ax.scatter3D(x_data, y_data, z_data, c=z_data, cmap='Greens')
+	plt.show()
+
+	exposed_sides = 0
+
+	x_max = max(x_data)
+	y_max = max(y_data)
+	z_max = max(z_data)
+
+
+	# initialize world
+	world = np.zeros((x_max+2, y_max+2, z_max+2))
+	for x, y, z in lava_droplets:
+		world[x][y][z] = 1
+	sys.setrecursionlimit(999999999)
+	lava(world, 0, 0, 0)
+
+	for x, y, z in lava_droplets:
+		exposed_sides += 1 if world[x+1][y][z] == 2 else 0
+		exposed_sides += 1 if world[x-1][y][z] == 2 else 0
+		exposed_sides += 1 if world[x][y+1][z] == 2 else 0
+		exposed_sides += 1 if world[x][y-1][z] == 2 else 0
+		exposed_sides += 1 if world[x][y][z+1] == 2 else 0
+		exposed_sides += 1 if world[x][y][z-1] == 2 else 0
+
+	print("Part 2: ", exposed_sides)
